c379ba1a3f5988db70a4b19fda86476268c12f2c
Abinash Gupta <P10433833@capita.co.in>
2018-05-09 16:26:28 +0530

    first modification

---
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import classes from './BuildControl.css';
+
+const buildControl = (props) => (
+    <div className={classes.BuildControl}>
+        <div className={classes.Label}>{props.label}</div>
+        <button 
+            className={classes.Less} 
+            onClick={props.removed} 
+            disabled={props.disabled}>Less</button>
+        <button 
+            className={classes.More} 
+            onClick={props.added}>More</button>
+        <p className={classes.IngPrice}>$ {(props.ingPrice * props.ingQty).toFixed(2)}</p>
+    </div>
+);
+
+export default buildControl;
